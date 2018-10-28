56e33148ee8c4ee441a88830fcd5c308002736ea
neerajpadarthi <42651065+neerajpadarthi@users.noreply.github.com>
2018-10-28 00:12:31 -0500

    ICP10

--- icp_10/public/script.js
+++ icp_10/public/script.js
@@ -22,23 +22,3 @@ function hideFunction() {
         x.style.display = "block";
     }
 }
-
-
-// 'use strict'
-// angular.module('know', [])
-//
-//     .controller('knowctrl', function($scope, $http) {
-//         $scope.a=false;
-//         $scope.getdetails = function () {
-//             $http.get("http://127.0.0.1:8083/servergoogle?searchtext="+$scope.search).then(function (t) {
-//                 console.log(t);
-//                 $scope.nam=t.data.itemListElement[0].result.name;
-//                 $scope.x = t.data.itemListElement[0].result.description;
-//                 $scope.y=t.data.itemListElement[0].result.detailedDescription.url;
-//                 $scope.a=true;
-//
-//             }).catch(function (err) {
-//                 console.log(err.message);
-//             })
-//         }
-//     })
--- icp_10/routes/index.js
+++ icp_10/routes/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var searchtext = req.query.searchtext;
-  var url = "https://kgsearch.googleapis.com/v1/entities:search?query="+searchtext+"&key=%20AIzaSyDMEBecZDKiws3eLRFIi1IO1E6Vm9s4Xj8%20&limit=1&indent=True";
+  var url = "https://kgsearch.googleapis.com/v1/entities:search?query="+searchtext+"&key=AIzaSyCZbMz2VUDfsNIawl7W9W64FpZp8gsoh10&limit=1&indent=True";
   request.get(url).pipe(res);
 });
 
